51efcd581add984f0fd69998c1473cdc5842d24c
jonathanunab <119382990+jonathanunab@users.noreply.github.com>
2022-12-17 23:34:43 +0000

    Se ajusta el frontend con las vistas requeridas y el Dockerfile

---
+++ src/conexiones/back.js
@@ -0,0 +1,5 @@
+module.exports = {
+    api: {
+        baseURL: "http://129.80.120.83:8080"
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["public/index.html","./src/**/*.{html,js}"],
+  content: ["public/index.html","./src/**/*.js"],
   theme: {
     extend: {},
   },
